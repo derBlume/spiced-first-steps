1faa199b42cf7666963870ec044205941be47267
Patrick Blume <71653434+derBlume@users.noreply.github.com>
2020-10-05 10:34:43 +0200

    cleanup

--- week01/Ex4-constructors/script.js
+++ week01/Ex4-constructors/script.js
@@ -53,9 +53,7 @@ console.log(string2);
 Write a constructor called Countdown that accepts a single argument - the number of seconds to count down. It should be possible to call the start method of instances of Countdown to initiate the countdown. Once the countdown starts, it should count down to zero starting with the number that was passed to the constructor and logging each number to the console with a one second delay. */
 
 function Countdown(time) {
-    this.time = time;
     this.start = function () {
-        var startTime = this.time;
         function simpleCountdown(timeLeft) {
             if (timeLeft >= 0) {
                 setTimeout(function () {
@@ -64,7 +62,7 @@ function Countdown(time) {
                 }, 1000);
             }
         }
-        simpleCountdown(startTime);
+        simpleCountdown(time);
     };
 }
 
