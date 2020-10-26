ab31d5f27cb0d920949920370abb1310bf4bb747
Patrick Blume <71653434+derBlume@users.noreply.github.com>
2020-10-26 12:08:43 +0100

    fun with fs, part 3 incomplete!

--- week04/Ex6-funWithFS/index.js
+++ week04/Ex6-funWithFS/index.js
@@ -26,7 +26,7 @@ function logSizes(path) {
     });
 }
 
-logSizes(path + "/files");
+//logSizes(path + "/files");
 
 function mapSizes(path) {
     let results = {};
@@ -57,3 +57,58 @@ function mapSizes(path) {
 let mapObj = mapSizes(path + "/files");
 
 fs.writeFileSync(path + "/files.json", JSON.stringify(mapObj, null, 4));
+
+function mapSizesAsync(path, cb, counter) {
+    let results = {};
+    let cbCounter = counter;
+    cbCounter++;
+    console.log("outer increased", cbCounter);
+    fs.readdir(path, { withFileTypes: true }, function (err, files) {
+        if (err) {
+            console.log("Reading directory didn't work.", err);
+            return;
+        } else {
+            files.forEach((element) => {
+                if (element.isFile()) {
+                    cbCounter++;
+                    console.log("inner increased", cbCounter);
+                    fs.stat(path + "/" + element.name, (err, stats) => {
+                        if (err) {
+                            console.log(("Getting stats didn't work.", err));
+                            return;
+                        } else {
+                            results[element.name] = stats.size;
+                            //console.log(results);
+                        }
+                        cbCounter--;
+                        console.log("inner decreased", cbCounter);
+                    });
+                } else if (element.isDirectory()) {
+                    mapSizesAsync(path + "/" + element.name, cb, cbCounter);
+                }
+            });
+        }
+        cbCounter--;
+        console.log("outer decreased", cbCounter);
+    });
+    if (cbCounter === 0) {
+        cb(results);
+    }
+}
+
+function actuallyWriteFile(results) {
+    console.log("callback called");
+    fs.writeFile(
+        path + "/filesAsync.json",
+        JSON.stringify(results, null, 4),
+        (err) => {
+            if (err) {
+                console.log(err);
+            }
+        }
+    );
+}
+
+mapSizesAsync(path + "/files", actuallyWriteFile, 0);
+//let mapObjAsync = mapSizesAsync(path + "/files");
+//console.log(JSON.stringify(mapSizesAsync(path + "/files"), null, 4));
