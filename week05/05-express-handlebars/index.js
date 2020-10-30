d18d6db7753e0ca25f2a67aa79062f75c85fe17b
Patrick Blume <71653434+derBlume@users.noreply.github.com>
2020-10-30 16:58:39 +0100

    project description pages added with partial

--- week05/05-express-handlebars/index.js
+++ week05/05-express-handlebars/index.js
@@ -4,54 +4,23 @@ const express = require("express");
 const app = express();
 
 const hb = require("express-handlebars");
+
 app.engine("handlebars", hb());
 app.set("view engine", "handlebars");
 
 app.listen(8080, () => console.log("Listening..."));
 
-app.use(require("cookie-parser")());
-
-// gives you request.body
-app.use(express.urlencoded({ extended: false }));
-
-app.use((request, response, next) => {
-    if (
-        request.cookies.acceptCookies === "true" ||
-        request.url === "/cookies" ||
-        request.url === "/favicon.ico"
-    ) {
-        next();
-    } else {
-        response.cookie("originalRoute", request.url);
-        response.redirect("/cookies");
-    }
-});
-
-app.get("/cookies", (request, response) => {
-    response.send(`
-    <h1>You must accept cookies to view this website</h1>
-        <form method='POST'>
-            <input type='checkbox' name='acceptCookies'>
-            <label for="acceptCookies">I accept all of the cookies!</label>
-            <button>submit</button>
-        </form>
-    `);
-});
-
-app.post("/cookies", (request, response) => {
-    if (request.body.acceptCookies === "on") {
-        response.cookie("acceptCookies", "true");
-        response.redirect(request.cookies.originalRoute);
-    } else {
-        response.send(
-            `<h1>Well, sorry. You can't continue without accepting cookies.</h1>`
-        );
-    }
-});
-
-app.get("/", (request, response) => {
-    //response.send(overview.create());
-    response.render("home", { projects });
-});
 app.use(express.static("projects"));
 app.use(express.static("public"));
+
+app.get("/", (request, response) => {
+    response.render("home", { projects });
+});
+
+app.get("/description/:projectName", (request, response) => {
+    for (let project of projects) {
+        if (project.dir === request.params.projectName) {
+            response.render("description", { project, projects });
+        }
+    }
+});
