18b0157ff0af00acbf626e071e0df8e0c10d6e66
Patrick Blume <71653434+derBlume@users.noreply.github.com>
2020-11-04 22:23:35 +0100

    testing Exercise 2 added

--- week06/04-testing/exercises/2/albums.test.js
+++ week06/04-testing/exercises/2/albums.test.js
@@ -4,6 +4,17 @@ const spotify = require("./spotify");
 jest.mock("./spotify");
 
 test("album names are in alphabetical order", () => {
+    spotify.search.mockResolvedValue({
+        albums: {
+            items: [
+                { name: "A Album" },
+                { name: "D Album" },
+                { name: "C Album" },
+                { name: "F Album" },
+                { name: "B Album" },
+            ],
+        },
+    });
     return getAlbumNames("meat loaf").then((albumNames) => {
         expect(albumNames).toEqual(albumNames.slice().sort());
     });
