36e05a4fa33ee68f20f351089c378e1cbc077a95
Patrick Blume <71653434+derBlume@users.noreply.github.com>
2020-11-05 17:15:28 +0100

    SQL exercise

---
+++ week06/05-database/actors.sql
@@ -0,0 +1,23 @@
+DROP TABLE IF EXISTS actors;
+DROP TABLE IF EXISTS cities;
+
+
+CREATE TABLE actors(
+  id SERIAL PRIMARY KEY,
+  full_name VARCHAR NOT NULL,
+  age INTEGER,
+  oscars INTEGER
+);
+
+INSERT INTO actors (full_name, age, oscars) VALUES ('Leonardo DiCaprio', 41, 1);
+INSERT INTO actors (full_name, age, oscars) VALUES ('Jennifer Lawrence', 25, 1);
+INSERT INTO actors (full_name, age, oscars) VALUES ('Samuel L. Jackson', 67, 0);
+INSERT INTO actors (full_name, age, oscars) VALUES ('Meryl Streep', 66, 3);
+INSERT INTO actors (full_name, age, oscars) VALUES ('John Cho', 43, 0);
+
+
+SELECT * FROM actors;
+
+SELECT * FROM actors WHERE oscars > 1;
+
+SELECT * FROM actors WHERE age > 30;
